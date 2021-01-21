49a40ead5f3079618b6b471292bb1c8521427e05
David Simek <david.simek1998@gmail.com>
2021-01-21 22:38:56 +0100

    created .env.sample + added load_sample_data command + some nit changes

--- django/app/ogm/apps.py
+++ django/app/ogm/apps.py
@@ -1,14 +1,13 @@
 from django.apps import AppConfig
 from py2neo import Graph
-from app.ogm import config
-
+from django.conf import settings
 
 
 graph = Graph(
-    host=config.NEO4J_HOST,
-    port=config.NEO4J_PORT,
-    user=config.NEO4J_USER,
-    password=config.NEO4J_PASSWORD,
+    host=settings.NEO4J_HOST,
+    port=settings.NEO4J_PORT,
+    user=settings.NEO4J_USER,
+    password=settings.NEO4J_PASSWORD,
 )
 
 
--- django/app/schema.py
+++ django/app/schema.py
@@ -1,31 +1,34 @@
 import graphene
 from graphene_django import DjangoObjectType
-
 from app.orm.models import SchoolLevel, SubjectCategory
 
 
-class CategoryType(DjangoObjectType):
+class SchoolLevelType(DjangoObjectType):
     class Meta:
-        model = Category
-        fields = ("id", "name", "ingredients")
+        model = SchoolLevel
+        fields = ("__all__")
 
-class IngredientType(DjangoObjectType):
+
+class SubjectCategoryType(DjangoObjectType):
     class Meta:
-        model = Ingredient
-        fields = ("id", "name", "notes", "category")
+        model = SubjectCategory
+        fields = ("__all__")
+
 
 class Query(graphene.ObjectType):
-    all_ingredients = graphene.List(IngredientType)
-    category_by_name = graphene.Field(CategoryType, name=graphene.String(required=True))
+    all_school_levels = graphene.List(SchoolLevelType)
+    subject_category_by_name = graphene.Field(SubjectCategoryType, name=graphene.String(required=True))
 
-    def resolve_all_ingredients(root, info):
-        # We can easily optimize query count in the resolve method
-        return Ingredient.objects.select_related("category").all()
+    @staticmethod
+    def resolve_all_school_levels(root, info):
+        return SchoolLevel.objects.all()
 
-    def resolve_category_by_name(root, info, name):
+    @staticmethod
+    def resolve_subject_category_by_name_by_name(root, info, name):
         try:
-            return Category.objects.get(name=name)
-        except Category.DoesNotExist:
+            return SubjectCategory.objects.get(name=name)
+        except SubjectCategory.DoesNotExist:
             return None
+
 
 schema = graphene.Schema(query=Query)
