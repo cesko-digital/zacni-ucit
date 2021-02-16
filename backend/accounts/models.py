64cb881bd0e948ff98bc2e9cb18b76b523ab8369
David Simek <david.simek1998@gmail.com>
2021-02-16 22:47:58 +0100

    ran python manage.py makemigrations + some nit changes based on PR

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,10 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
-    USERNAME_FIELD = EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
-    email = models.EmailField(_("email address"), unique=True)
-
-    class Meta:
-        db_table = "auth_user"
+    email = models.EmailField("email address", unique=True)
