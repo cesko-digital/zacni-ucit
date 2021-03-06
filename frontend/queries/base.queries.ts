2e74929c830795b76903622f8301d1a8971d6288
Jiří Petera <jiripetera@Jiri-MacBook-Pro.local>
2021-03-06 23:11:47 +0100

    Feat(Adding Queries, creating preview)

---
+++ frontend/queries/base.queries.ts
@@ -0,0 +1,74 @@
+import { gql } from '@apollo/client';
+
+export const allCollegesQuery = gql`
+  query {
+    colleges {
+      id
+      name
+      type
+      form
+      address
+      rid
+      ic
+      databox
+      url
+      code
+    }
+  }
+`;
+
+export const allTitlesQuery = gql`
+  query getTitles {
+    titles {
+      id
+      code
+      name
+    }
+  }
+`;
+
+export const allSchoolLevelsQuery = gql`
+  query allSchoolLevelsQuery {
+    schoolLevels {
+      id
+      name
+    }
+  }
+`;
+
+export const allSubjectsQuery = gql`
+  query allSubjectsQuery {
+    subjects {
+      id
+      code
+      name
+    }
+  }
+`;
+
+export const titleQuery = gql`
+  query title($id: Integer!) {
+    title(id: $id) {
+      id
+      code
+      name
+    }
+  }
+`;
+
+export const collegeQuery = gql`
+  query college($id: Integer!) {
+    college(id: $id) {
+      id
+      name
+      type
+      form
+      address
+      rid
+      ic
+      databox
+      url
+      code
+    }
+  }
+`;
