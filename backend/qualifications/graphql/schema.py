816c3ac0028eca4d1640f9fe90bc2647649d91c4
David Simek <david.simek1998@gmail.com>
2021-03-06 21:02:09 +0100

    BE added collageAreas queries

--- backend/qualifications/graphql/schema.py
+++ backend/qualifications/graphql/schema.py
@@ -1,11 +1,13 @@
 import graphene
-from qualifications.graphql.types import TitleType
-from qualifications.models import Title
+from qualifications.graphql.types import TitleType, CollegeAreaType
+from qualifications.models import Title, CollegeArea
 
 
 class Query(graphene.ObjectType):
     titles = graphene.List(TitleType)
     title = graphene.Field(TitleType, pk=graphene.Int(required=True))
+    college_areas = graphene.List(CollegeAreaType)
+    college_area = graphene.Field(CollegeAreaType, pk=graphene.Int(required=True))
 
     @staticmethod
     def resolve_titles(root, info):
@@ -14,3 +16,11 @@ class Query(graphene.ObjectType):
     @staticmethod
     def resolve_title(root, info, pk: int):
         return Title.objects.get(pk=pk)
+
+    @staticmethod
+    def resolve_college_areas(root, info):
+        return CollegeArea.objects.all()
+
+    @staticmethod
+    def resolve_collage_area(root, info, pk: int):
+        return CollegeArea.objects.get(pk=pk)
