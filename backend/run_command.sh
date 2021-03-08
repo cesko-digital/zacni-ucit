84314ca315969cf22c5ba0bc36ba9e0d706894bb
Martin Wenisch <martin.wenisch@seznam.cz>
2021-03-08 21:24:47 +0100

    Infrastructure + deployment (#38)
    * Backend and Frontend deployment
    * Terraform infrastructure

---
+++ backend/run_command.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python /app/manage.py migrate && python /app/manage.py runserver 0.0.0.0:8300
---
+++ infrastructure/aws/variables.tf
@@ -0,0 +1,42 @@
+variable "aws-region" {
+  type    = string
+  default = "eu-central-1"
+}
+
+variable "codename" {
+  type    = string
+  default = "zacniucit"
+}
+
+# Internal domain, its not visible publicly, but it should be domain we own
+# to comply with best practices => it ensures the internal domain names are globally unique.
+variable "codename-domain" {
+  type    = string
+  default = "zacniucit.cz"
+}
+
+variable "database-username" {
+  type    = string
+  default = "zacniucitdevelopment"
+}
+
+variable "database-password" {
+  type = string
+}
+
+variable "neo4j-password" {
+  type = string
+}
+
+variable "django-secret" {
+  type = string
+}
+
+variable "sendgrid-api-key" {
+  type = string
+}
+
+variable "development-frontend-bucket-name" {
+  type    = string
+  default = "zacnicuit-frontend-development"
+}
