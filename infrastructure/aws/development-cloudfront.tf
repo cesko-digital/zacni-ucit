f31e6e7977e0f485137e687db8f636c8856e9052
Jakub Bares <bares.jakub@gmail.com>
2021-04-10 20:31:58 +0200

    Finalni funkcni verze

--- infrastructure/aws/development-cloudfront.tf
+++ infrastructure/aws/development-cloudfront.tf
@@ -5,20 +5,10 @@ resource "aws_cloudfront_origin_access_identity" "default" {
 
 resource "aws_cloudfront_distribution" "frontend_cloudfront" {
   origin {
-    domain_name = "${aws_s3_bucket.frontend.bucket}.${aws_s3_bucket.frontend.website_domain}"
-    origin_id   = aws_s3_bucket.frontend.id
+    domain_name = "${aws_s3_bucket.frontend.bucket_domain_name}"
+    origin_id   = "S3-Website-${aws_s3_bucket.frontend.bucket}.${aws_s3_bucket.frontend.website_domain}"
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.default.cloudfront_access_identity_path
-    }
-    custom_origin_config {
-      http_port              = 80
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = [
-        "TLSv1",
-        "TLSv1.1",
-        "TLSv1.2"
-      ]
     }
   }
 
@@ -49,40 +39,6 @@ resource "aws_cloudfront_distribution" "frontend_cloudfront" {
     default_ttl            = 86400
     max_ttl                = 86400
     compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/api/v1/*"
-    allowed_methods  = [
-      "GET",
-      "HEAD",
-      "OPTIONS",
-      "PUT",
-      "POST",
-      "PATCH",
-      "DELETE"]
-    cached_methods   = [
-      "GET",
-      "HEAD"]
-    target_origin_id = "backend-id"
-
-    forwarded_values {
-      query_string = true
-      headers      = [
-        "Authorization",
-        "X-ApplicationId"
-      ]
-      cookies {
-        forward = "all"
-      }
-    }
-    min_ttl                = 0
-    default_ttl            = 0
-    max_ttl                = 0
-    compress               = true
-    smooth_streaming       = true
     viewer_protocol_policy = "redirect-to-https"
   }
 
