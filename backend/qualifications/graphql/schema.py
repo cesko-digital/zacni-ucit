eeadea94fdfeb97caf915eddb98ca3a171d2106f
David Simek <david.simek1998@gmail.com>
2021-04-22 20:03:05 +0100

    added needToStudy endpoint + some some more ObjectTypes

--- backend/qualifications/graphql/schema.py
+++ backend/qualifications/graphql/schema.py
@@ -1,6 +1,9 @@
 import graphene
-from qualifications.graphql.types import TitleObjectType, CollegeAreaObjectType
-from qualifications.models import Title, CollegeArea
+from qualifications.graphql.types import TitleObjectType, CollegeAreaObjectType, CollegeProgrammeObjectType, \
+    EducationAreaObjectType, PreparationTypeObjectType, SubjectTypeObjectType, EducationTypeObjectType, \
+    OtherOptionObjectType
+from qualifications.models import Title, CollegeArea, CollegeProgramme, EducationArea, PreparationType, SubjectType, \
+    OtherOption
 
 
 class Query(graphene.ObjectType):
@@ -11,6 +14,30 @@ class Query(graphene.ObjectType):
     # College areas queries
     college_areas = graphene.List(CollegeAreaObjectType)
     college_area = graphene.Field(CollegeAreaObjectType, pk=graphene.Int(required=True))
+
+    # College Programme queries
+    college_programmes = graphene.List(CollegeProgrammeObjectType)
+    college_programme = graphene.Field(CollegeProgrammeObjectType, pk=graphene.Int(required=True))
+
+    # Education area queries
+    education_areas = graphene.List(EducationAreaObjectType)
+    education_area = graphene.Field(EducationAreaObjectType, pk=graphene.Int(required=True))
+
+    # Preparation type queries
+    preparation_types = graphene.List(PreparationTypeObjectType)
+    preparation_type = graphene.Field(PreparationTypeObjectType, pk=graphene.Int(required=True))
+
+    # Subject type queries
+    subject_types = graphene.List(SubjectTypeObjectType)
+    subject_type = graphene.Field(SubjectTypeObjectType, pk=graphene.Int(required=True))
+
+    # Education type queries
+    education_types = graphene.List(EducationTypeObjectType)
+    education_type = graphene.Field(EducationTypeObjectType, pk=graphene.Int(required=True))
+
+    # Other option queries
+    other_options = graphene.List(OtherOptionObjectType)
+    other_option = graphene.List(OtherOptionObjectType, pk=graphene.Int(required=True))
 
     @staticmethod
     def resolve_titles(root, info):
@@ -27,3 +54,51 @@ class Query(graphene.ObjectType):
     @staticmethod
     def resolve_collage_area(root, info, pk: int):
         return CollegeArea.objects.get(pk=pk)
+
+    @staticmethod
+    def resolve_college_programmes(root, info):
+        return CollegeProgramme.objects.all()
+
+    @staticmethod
+    def resolve_college_programme(root, info, pk: int):
+        return CollegeProgramme.objects.get(pk=pk)
+
+    @staticmethod
+    def resolve_education_areas(root, info):
+        return EducationArea.objects.all()
+
+    @staticmethod
+    def resolve_education_area(root, info, pk: int):
+        return EducationArea.objects.get(pk=pk)
+
+    @staticmethod
+    def resolve_preparation_types(root, info):
+        return PreparationType.objects.all()
+
+    @staticmethod
+    def resolve_preparation_type(root, info, pk: int):
+        return PreparationType.objects.get(pk=pk)
+
+    @staticmethod
+    def resolve_subject_types(root, info):
+        return SubjectType.objects.all()
+
+    @staticmethod
+    def resolve_subject_type(root, info, pk: int):
+        return SubjectType.objects.get(pk=pk)
+
+    @staticmethod
+    def resolve_education_types(root, info):
+        return SubjectType.objects.all()
+
+    @staticmethod
+    def resolve_education_type(root, info, pk: int):
+        return SubjectType.objects.get(pk=pk)
+
+    @staticmethod
+    def resolve_other_options(root, info):
+        return OtherOption.objects.all()
+
+    @staticmethod
+    def resolve_other_option(root, info, pk: int):
+        return OtherOption.objects.get(pk=pk)
--- backend/teaching/graphql/schema.py
+++ backend/teaching/graphql/schema.py
@@ -1,4 +1,7 @@
 import graphene
+
+from colleges.graphql.types import CourseObjectType
+from colleges.models import Course
 from teaching.graphql.types import SchoolLevelObjectType, SubjectObjectType
 from teaching.models import SchoolLevel, Subject
 
@@ -6,7 +9,19 @@ from teaching.models import SchoolLevel, Subject
 class Query(graphene.ObjectType):
     school_levels = graphene.List(SchoolLevelObjectType)
     subjects = graphene.List(SubjectObjectType, school_level_ids=graphene.List(graphene.Int, required=False))
-    want_to_teach_result = graphene.String()
+    need_to_study = graphene.List(
+        CourseObjectType,
+        school_level_ids=graphene.List(graphene.Int),
+        subjects_ids=graphene.List(graphene.Int),
+        title_ids=graphene.List(graphene.Int),
+        college_programme_ids=graphene.List(graphene.Int, required=False),
+        college_area_ids=graphene.List(graphene.Int, required=False),
+        education_area_ids=graphene.List(graphene.Int, required=False),
+        preparation_type_ids=graphene.List(graphene.Int, required=False),
+        subjects_type_ids=graphene.List(graphene.Int, required=False),
+        education_type_ids=graphene.List(graphene.Int, required=False),
+        other_option_ids=graphene.List(graphene.Int, required=False)
+    )
 
     @staticmethod
     def resolve_school_levels(root, info):
@@ -19,7 +34,18 @@ class Query(graphene.ObjectType):
         return Subject.objects.all()
 
     @staticmethod
-    def resolve_want_to_teach_result(root, info):
-        # TODO We need to discuss how this result should work.
-        #  We also need some cypher queries, to get the data from Neo4J
-        return "Everything"
+    def resolve_need_to_study(
+            root,
+            info,
+            school_level_ids: list,
+            subjects_ids: list,
+            title_ids: list,
+            college_programme_ids: list = None,
+            college_area_ids: list = None,
+            education_area_ids: list = None,
+            preparation_type_ids: list = None,
+            subjects_type_ids: list = None,
+            other_option_ids: list = None
+    ):
+        # TODO add cypher query for and filter courses based on result
+        return Course.objects.all()
