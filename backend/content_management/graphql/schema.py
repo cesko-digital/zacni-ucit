388692061b3e2dfe0daa94ab8fb7c7c775932e90
Mia <mia_bajic@live.com>
2021-08-03 14:38:11 +0100

    created new graphql endpoint

---
+++ backend/content_management/graphql/schema.py
@@ -0,0 +1,20 @@
+import graphene
+from .models import WebSiteText
+
+
+class Query(graphene.ObjectType):
+    texts = graphene.List(WebSiteTextType)
+    text_by_page_id = graphene.Field(WebSiteTextType, pk=graphene.Int(required=True))
+    text_by_element_id = graphene.Field(WebSiteTextType, pk=graphene.Int(required=True))
+
+    @staticmethod
+    def resolve_texts(root, info):
+        return WebSiteText.objects.all()
+
+    @staticmethod
+    def resolve_text_by_page_id(root, info, pk:int):
+        return WebSiteText.objects.get(pk=pk)
+
+    @staticmethod
+    def resolve_text_by_element_id(root, info, pk:int):
+        return WebSiteText.objects.get(pk=pk)
--- backend/zacniucit/schema.py
+++ backend/zacniucit/schema.py
@@ -5,6 +5,7 @@ import teaching.graphql.schema
 import qualifications.graphql.schema
 import accounts.graphql.mutations
 import teaching.graphql.mutations
+import content_management.graphql.schema
 
 from graphql_auth.schema import MeQuery
 
@@ -14,6 +15,7 @@ class Query(
     qualifications.graphql.schema.Query,
     colleges.graphql.schema.Query,
     teaching.graphql.schema.Query,
+    content_management.graphql.schema.Query,
     graphene.ObjectType,
 ):
     is_alive = graphene.Boolean(description="Base API test")
