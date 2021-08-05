707544905eebb1f81f9b69947ff99859ac8cd814
Mia <mia_bajic@live.com>
2021-08-05 13:49:58 +0100

    removed all texts, fixed error with import

--- backend/content_management/graphql/schema.py
+++ backend/content_management/graphql/schema.py
@@ -1,15 +1,11 @@
 import graphene
-from .models import WebSiteText
+from content_management.models import WebSiteText
+from content_management.graphql.types import WebSiteTextType
 
 
 class Query(graphene.ObjectType):
-    texts = graphene.List(WebSiteTextType)
     text_by_page_id = graphene.Field(WebSiteTextType, pk=graphene.Int(required=True))
     text_by_element_id = graphene.Field(WebSiteTextType, pk=graphene.Int(required=True))
-
-    @staticmethod
-    def resolve_texts(root, info):
-        return WebSiteText.objects.all()
 
     @staticmethod
     def resolve_text_by_page_id(root, info, pk: int):
