2c7874e3d356c8ef2725624b272d3203dffd0af6
Marcel Jäger <mjager9@gmail.com>
2021-08-13 09:47:54 +0200

    add @types/styled-components

--- frontend/next-env.d.ts
+++ frontend/next-env.d.ts
@@ -1,14 +1,12 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 
+declare module 'styled-components';
+
 declare module '*.png' {
   const value: string;
   export default value;
 }
-
-declare module 'styled.ts';
-declare module 'styled.tsx';
-declare module 'styled';
 
 declare module '*.jpg' {
   const value: string;
