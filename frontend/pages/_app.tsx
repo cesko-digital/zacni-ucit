3c867cd4dec432493fd75efe9f2f2caacd3e5d88
Marcel Jager <marcel.jager@cdn77.com>
2021-08-15 16:34:43 +0200

    fix server side style generating

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -14,14 +14,6 @@ import Layout from '@components/Layout/Layout';
 const MyApp = ({ Component, pageProps }) => {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
   return (
     <>
       <Head>
--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,22 +1,30 @@
-import { ServerStyleSheets } from '@material-ui/core/styles';
+import { ServerStyleSheet } from 'styled-components';
 import Document from 'next/document';
 import React from 'react';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheets = new ServerStyleSheets();
+    const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: App => props => sheets.collect(<App {...props} />),
-      });
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
 
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      styles: [sheets.getStyleElement(), ...React.Children.toArray(initialProps.styles)],
-    };
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 }
