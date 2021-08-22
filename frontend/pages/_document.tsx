f49eac1e23d617804e9be5506318a2fb8a1b3000
Marcel Jager <marcel.jager@cdn77.com>
2021-08-22 09:18:12 +0200

    add GTM, disable "Pruvodce" from production

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,6 +1,8 @@
 import { ServerStyleSheet } from 'styled-components';
-import Document from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
+
+const G_TAG_ID = 'UA-153416106-1';
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -26,5 +28,32 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${G_TAG_ID}`} />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${G_TAG_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
--- frontend/pages/pruvodce.tsx
+++ frontend/pages/pruvodce.tsx
@@ -1,3 +1,12 @@
 import Configurator from '@pages/Configurator/Configurator';
+import Error404 from '@pages/Errors/Error404/Error404';
 
-export default Configurator;
+const Page: React.FC = () => {
+  if (process.env.NODE_ENV === 'development') {
+    return <Configurator />;
+  }
+
+  return <Error404 />;
+};
+
+export default Page;
