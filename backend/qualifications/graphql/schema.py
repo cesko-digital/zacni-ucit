66c1c8044911d4f43c7640b8db04c6f37509ab1b
Michael Sterzinger <69076772+idep-nter@users.noreply.github.com>
2021-08-31 21:55:05 +0200

    Update schema.py

--- backend/qualifications/graphql/schema.py
+++ backend/qualifications/graphql/schema.py
@@ -70,7 +70,7 @@ class Query(graphene.ObjectType):
         # query na vyfiltrovani cest + join s education types
         subject_group = SubjectGroup.objects.filter(subject__name=subject)
         paths = Qualification.objects.filter(subject_group=subject_group, school_level=school_level).prefetch_related(
-            "education_types").values()
+            "education_types")
 
         # prevedeni na pandas a pridani sloupce completed do kazde EduType na zaklade user inputu
         df = pd.DataFrame(paths)
