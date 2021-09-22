07ca984ad7a2ac9d4741261017168ae4e72e52fd
Marcel Jäger <mjager9@gmail.com>
2021-09-22 08:17:20 +0200

    add annotation to workshops page

--- frontend/src/pages/LearnPage/Workshops/Workshops.tsx
+++ frontend/src/pages/LearnPage/Workshops/Workshops.tsx
@@ -11,7 +11,9 @@ import {
   EventTitle,
   TopWrapper,
   Paragraph,
-  EventDateContainer,
+  EventDateTime,
+  EventTime,
+  EventDescription,
 } from '../styled';
 import BackToHomeButton from '@components/BackToHomeButton/BackToHomeButton';
 import Button from '@components/Button/Button';
@@ -27,6 +29,7 @@ type Event = {
   date: string;
   time: string;
   title: string;
+  text: string;
 };
 
 const events: Event[] = [
@@ -34,21 +37,25 @@ const events: Event[] = [
     date: '30. 9. 2021',
     time: '18:00 - 19:30',
     title: 'Kateřina Vrtišková: Jak přemýšlet nad hodnocením',
+    text: 'Kdo, co a jak má ve škole hodnotit? V průběhu tohoto workshopu se dozvíte, jak nad hodnocením přemýšlet a jak se při hodnocení zaměřit na to důležité. Dozvíte se nejen o hodnocení od učitele směrem k žákům, ale i o tom, jak nechat děti, aby hodnotily samy sebe nebo své spolužáky. ',
   },
   {
     date: '14. 10. 2021',
     time: '18:00 - 19:30',
     title: 'Marcela Roflíková:  Práce s hranicemi',
+    text: 'Jak to mám v životě s hranicemi já? Umím říci NE? Kde narážím na hranice druhých lidí? Hranice určují kým jsme a kým nejsme. Dovolují a pomáhají nám udržovat si komfortní zónu a tím udržovat optimální výkonnost a motivaci k tomu, co děláme. Hranice jsou proměnlivé. Naše aktivní a vědomá práce s hranicemi nám přináší kvalitnější vztahy se svým okolím, s kolegy i dětmi. Všímavost (mindfulness) nám pomáhá uvědomovat si, kdy jsou naše hranice překračovány a narušovány.',
   },
   {
     date: '11. 11. 2021',
     time: '18:00 - 19:30',
     title: 'Ina Rajsiglová: Aktivizační metody do výuky',
+    text: 'Jaké metody zvolit, aby byli naši žáci aktivní? V průběhu tohoto workshopu si sami v roli žáků vyzkoušíte několik metod, které vedou k samostatnosti a schopnosti učit se. Také se budete společně přemýšlet o smyslu a smysluplném zařazení těchto metod do výuky.',
   },
   {
     date: '2. 12. 2021',
     time: '18:00 - 19:30',
     title: 'Pavlína Seidlerová: Práce s chybou',
+    text: 'Jaký je můj vztah k chybě a jak to ovlivňuje moji praxi? Co to je chyba v procesu učení? Jak reagovat na chybu, aby to podpořilo další učení žáků? Jak můžeme společně se žáky chybu vytěžit pro učení? Seznámíme s více jak desítkou různých možností, jak reagovat v písemných pracích na chyby a budeme hledat výhody a nevýhody těchto možností.',
   },
 ];
 
@@ -93,16 +100,15 @@ const Workshops: React.FC = () => (
       </Section>
     </div>
     <div>
-      {events.map(({ date, time, title }) => (
-        <Event key={date} time>
-          <EventDateContainer>
+      {events.map(({ date, time, title, text }) => (
+        <Event key={date}>
+          <EventDateTime>
             <EventDate>{date}</EventDate>
-            <EventDate time className="time">
-              {time}
-            </EventDate>
-          </EventDateContainer>
+            {time && <EventTime>{time}</EventTime>}
+          </EventDateTime>
           <EventContent>
-            <EventTitle selfContained>{title}</EventTitle>
+            <EventTitle>{title}</EventTitle>
+            <EventDescription>{text}</EventDescription>
           </EventContent>
         </Event>
       ))}
