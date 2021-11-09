830e354674a1dcf510a0d4d55dbdc37ce426aa87
fikymar <67013637+fikymar@users.noreply.github.com>
2021-11-09 10:37:13 +0100

    add msmt on hp, footer (#116)

--- frontend/src/pages/HomePage/PartnersSection/PartnersSection.tsx
+++ frontend/src/pages/HomePage/PartnersSection/PartnersSection.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-import { Section, MainTitle, LogosWrapper, LogoItem, Link, Logo } from './styled';
+import {
+  Section,
+  MainTitle,
+  LogosWrapper,
+  LogoItem,
+  Link,
+  Logo,
+  TextWrapper,
+  MsmtWrapper,
+  LogoContainer,
+} from './styled';
 import Button from '@components/Button/Button';
 import Container from '@components/Container/Container';
 import { routes } from '@routes';
-
+import Image from 'next/image';
 import ceskoDigitalLogo from './cesko-digital.png';
 import laboratorVodafoneLogo from './laborator-vodafone.png';
 import msmtLogo from './msmt.png';
@@ -34,6 +44,16 @@ const PartnersSection: React.FC = () => (
           </LogoItem>
         ))}
       </LogosWrapper>
+      <MsmtWrapper>
+        <TextWrapper>
+          <p>Projekt Začni učit! má </p>{' '}
+          <p>
+            <strong>záštitu od MŠMT ČR.</strong>
+          </p>
+        </TextWrapper>
+
+        <Image alt="msmt" height={62} src={msmtLogo} width={132} />
+      </MsmtWrapper>
       <Button buttonStyle="button" href={routes.partners}>
         Chci se stát partnerem
       </Button>
