c2863b460f3e23f5f4a3c2f9a48bd5fbb78b61d7
Marcel Jager <marcel.jager@cdn77.com>
2021-12-05 19:19:44 +0100

    add trailing slash to graphql url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,5 +2,5 @@ export default {
   backendUri:
     process.env.NODE_ENV === 'development'
       ? 'http://zacniucit-elb-406a2c6714d8ca1b.elb.eu-central-1.amazonaws.com:8300/graphql/'
-      : '/graphql',
+      : '/graphql/',
 };
